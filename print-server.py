3fb9d5d530cb136d29a9231fb0f26baba07deb70
JochemDeGroot <jdegroot@exyt.es>
2023-06-01 12:26:11 +0200

    :wrench: refactor: change reference names and add error case

--- print-server.py
+++ print-server.py
@@ -1,31 +1,63 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from escpos import *
+from fastapi.responses import JSONResponse
+from typing import Optional
 
 app = FastAPI()
 
-class PrintData(BaseModel):
-    turn: str
-    seat: str
-    qr: str
+origins = ['*']
 
-@app.post("/print-data")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class PrintData(BaseModel):
+    phone: Optional[str]
+    note: Optional[str]
+    code: Optional[str]
+    name: Optional[str]
+    collective: Optional[str]
+    identifier: Optional[str]
+    status: Optional[str]
+    email: Optional[str]
+
+
+
+@app.post("/print")
 def info_to_print(data: PrintData):
     
-    p = printer.Serial("COM5")
+    try:
+        p = printer.Serial("COM5")
+    except Exception as e:
+        print(e)
+        return JSONResponse(
+        status_code=404,
+        content={"context": "no_printer_found"},
+        )
+
+
     p.magic.force_encoding("CP858")
 
     p.set(align="center", custom_size=True, width=2, height=2)
-    p.text(data.turn)
+    p.text(data.phone)
     p.ln()
     p.ln()
     p.set(align="center", custom_size=True, width=1, height=1)
-    p.qr(data.qr, size=8)
+    p.qr(data.code, size=8)
     p.ln()
     p.ln()
     p.set(align="center", custom_size=True, width=2, height=2)
-    p.text(data.seat)
+    p.text(data.note)
     p.ln()
     p.cut()
 
-    return {"Status": "Correct"}
+    return JSONResponse(
+        status_code=200,
+        content={"context": "printing_ticket"},
+        )
